a9d8115e9392b302d56ec6a85a92193abb17e2c5
Domonkos <bdomonkosinf@gmail.com>
2024-06-20 12:06:12 +0200

    Feature: add get vehicles in radius logic -Added native query to VehicleRepository to retrieve vehicles based on position and radius -Integrated the new logic into the existing vehicle service. -Implement endpoint in Vehicle controller for retrieving vehicles by position and radius

--- vehicle-simulator-backend/src/main/java/com/vechile/simulator/repository/VehicleRepository.java
+++ vehicle-simulator-backend/src/main/java/com/vechile/simulator/repository/VehicleRepository.java
@@ -15,4 +15,24 @@ import org.springframework.stereotype.Repository;
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
 
+  /**
+   * Retrieves vehicles within a specified radius from a given latitude and longitude.
+   *
+   * @param latitude   The latitude coordinate.
+   * @param longitude  The longitude coordinate.
+   * @param radiusInKm The radius in kilometers.
+   * @return The list of vehicles within the specified radius.
+   */
+
+  @Query(value = "SELECT * \n"
+      + "FROM vehicle v \n"
+      + "WHERE ST_Distance(\n"
+      + "cast(ST_SetSRID(ST_MakePoint(v.latitude, v.longitude), 4326) as geography), \n"
+      + "cast(ST_SetSRID(ST_MakePoint(:latitude, :longitude), 4326) as geography)\n"
+      + ") < (:radius * 1000)", nativeQuery = true)
+  List<Vehicle> findVehiclesInCircle(
+      @Param("latitude") double latitude,
+      @Param("longitude") double longitude,
+      @Param("radius") double radiusInKm
+  );
 }
